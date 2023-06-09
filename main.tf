644392d7a7c34a6d40d778a63ce48d7f7298cd75
cedric69lt <127845795+cedric69lt@users.noreply.github.com>
2023-06-09 10:48:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "azurerm" {
    subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
-   client_id = "384ba4e4-fc1a-47f4-bf42-acec6fad1e82"
-   client_secret = "lxk8Q~LfBuObGY4frVfc6B_e4Nqf6UMahS-BdaD-"
+   client_id = ""
+   client_secret = ""
    tenant_id = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
    skip_provider_registration = true
    features {}
