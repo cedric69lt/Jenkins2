03739d0ba6b20adfeeaf93cb591940f465273b3b
cedric69lt <127845795+cedric69lt@users.noreply.github.com>
2023-06-09 12:11:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,6 +63,22 @@ resource "azurerm_network_security_group" "allowedports" {
    }
 }
 
+resource "azurerm_virtual_network" "webserver-net" {
+  name                = "webserver-net"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.webserver.location
+  resource_group_name = azurerm_resource_group.webserver.name
+}
+
+resource "azurerm_subnet" "webserver-subnet" {
+  name                 = "subnet01"
+  resource_group_name  = azurerm_resource_group.webserver.name
+  virtual_network_name = azurerm_virtual_network.webserver-net.name
+  address_prefixes       = ["10.0.1.0/24"]
+
+  private_link_service_network_policies_enabled = false
+}
+
 resource "azurerm_subnet_network_security_group_association" "mgmt-nsg-association" {
     subnet_id = azurerm_subnet.webserver-subnet.id
     network_security_group_id = azurerm_network_security_group.allowedports.id
