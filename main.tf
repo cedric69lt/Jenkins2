bb34e1e046eb6fcc530908231515d4f28d25ada2
cedric69lt <127845795+cedric69lt@users.noreply.github.com>
2023-06-09 11:45:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,112 @@ resource "azurerm_resource_group" "test" {
   name     = "cedrictestTerraform"
   location = "francecentral"
 }
+
+
+resource "azurerm_network_security_group" "allowedports" {
+   name = "allowedports"
+   resource_group_name = azurerm_resource_group.test.name
+   location = azurerm_resource_group.test.location
+  
+   security_rule {
+       name = "http"
+       priority = 100
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "80"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+
+   security_rule {
+       name = "https"
+       priority = 200
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "443"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+
+   security_rule {
+       name = "ssh"
+       priority = 300
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "22"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+}
+
+resource "azurerm_virtual_network" "webserver-net" {
+  name                = "webserver-net"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_subnet" "webserver-subnet" {
+  name                 = "subnet01"
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.webserver-net.name
+  address_prefixes       = ["10.0.1.0/24"]
+
+  private_link_service_network_policies_enabled = false
+}
+
+resource "azurerm_public_ip" "webserver_public_ip" {
+   name = "webserver_public_ip"
+   location = "francecentral"
+   resource_group_name = azurerm_resource_group.test.name
+   allocation_method = "Dynamic"
+}
+
+resource "azurerm_network_interface" "webserver" {
+   name = "nginx-interface"
+   location = "francecentral"
+   resource_group_name = azurerm_resource_group.test.name
+
+   ip_configuration {
+       name = "internal"
+       private_ip_address_allocation = "Dynamic"
+       subnet_id = azurerm_subnet.webserver-subnet.id
+       public_ip_address_id = azurerm_public_ip.webserver_public_ip.id
+   }
+
+   depends_on = [azurerm_resource_group.webserver]
+}
+
+resource "azurerm_linux_virtual_machine" "nginx" {
+   size = "Standard_F2"
+   name = "nginx-webserver"
+   resource_group_name = azurerm_resource_group.test.name
+   location = azurerm_resource_group.test.location
+   network_interface_ids = [
+       azurerm_network_interface.webserver.id,
+   ]
+
+   source_image_reference {
+       publisher = "Canonical"
+       offer = "UbuntuServer"
+       sku = "18.04-LTS"
+       version = "latest"
+   }
+
+   computer_name = "nginx"
+   admin_username = "azureuser"
+   admin_password = "Azertyty123!"
+   disable_password_authentication = false
+
+   os_disk {
+       name = "nginxdisk01"
+       caching = "ReadWrite"
+       #create_option = "FromImage"
+       storage_account_type = "Standard_LRS"
+   }
